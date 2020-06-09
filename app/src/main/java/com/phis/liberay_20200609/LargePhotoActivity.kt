57ee428ad27c3e43c8a917f367bc455fd3aa8c96
kswiiz0 <kswiiz0@gmail.com>
2020-06-09 20:13:00 +0900

    기능추가  - 사진크게 보기  > 핀치 줌/ 더블더치 대응 필요.

---
+++ app/src/main/java/com/phis/liberay_20200609/LargePhotoActivity.kt
@@ -0,0 +1,21 @@
+package com.phis.liberay_20200609
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LargePhotoActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_large_photo)
+    }
+
+    override fun setValues() {
+
+    }
+
+    override fun setupEvents() {
+        
+    }
+}
--- app/src/main/java/com/phis/liberay_20200609/MainActivity.kt
+++ app/src/main/java/com/phis/liberay_20200609/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.phis.liberay_20200609
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setValues()
+        setupEvents()
     }
 
     override fun setValues() {
@@ -14,6 +19,12 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        profileIV.setOnClickListener({
+            val myIntent = Intent(mContext,LargePhotoActivity::class.java)
+            startActivity(myIntent)
+
+        })
 
     }
 
